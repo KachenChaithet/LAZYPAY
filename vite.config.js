4e706aecd558a4327d3e52b4e95857f77ad7fdbe
Kchen.github.io <chaytheskhchenthr@gmail.com>
2025-09-29 18:46:40 +0700

    edit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  preview: {
+    port: parseInt(process.env.PORT) || 10000,
+    host: '0.0.0.0'
+  }
 })
