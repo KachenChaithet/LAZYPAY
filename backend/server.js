4155ee246c9b3d1b81a5e6b6271cde65e71334f8
Kchen.github.io <chaytheskhchenthr@gmail.com>
2025-09-29 17:25:57 +0700

    LAZYPAY

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import pkg from 'promptpay-qr';
+import QRCode from 'qrcode';
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+const app = express()
+dotenv.config()
+
+app.use(express.json())
+app.use(cors({ origin: 'http://localhost:5173' }))
+
+
+const port = process.env.PORT
+const generatePayload =
+    pkg.generatePayload || pkg.default?.generatePayload || pkg
+
+app.post('/generate-qr', async (req, res) => {
+    try {
+        const { amount } = req.body
+
+        if (!amount) {
+            return res.status(400).json({ error: "ต้องใส่ phoneNumber และ amount" })
+        }
+
+        const payload = generatePayload(process.env.PHONENUMBER, { amount: parseFloat(amount) })
+
+        // แปลง payload เป็น base64 image ส่งกลับไป
+        const qrImage = await QRCode.toDataURL(payload, {
+            color: {
+                dark: "#000000",
+                light: "#FFFFFF",
+            },
+            width: 300,
+            margin: 2,
+        })
+
+        res.json({ qr: qrImage })
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ error: "ไม่สามารถสร้าง QR ได้" })
+    }
+})
+
+app.listen(port, () => {
+    console.log('server run on port', port);
+
+})
---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { AwardIcon, Download } from 'lucide-react'
+import './index.css'
+import { useState } from 'react'
+import axios from 'axios'
+
+
+function App() {
+  const [Amount, setAmount] = useState()
+  const [qr, setQr] = useState('')
+
+  const handleCreateQrCode = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/generate-qr', { amount: Amount })
+      setQr(response.data)
+    } catch (error) {
+      return console.log(error);
+
+    }
+  }
+
+  return (
+    <>
+      <div className="h-screen flex justify-start gap-8 mt-10 items-center flex-col ">
+        <div className="flex flex-col items-center gap-4">
+          <h1 className='text-4xl font-semibold text-blue-900'>นี่คือqr code กูเอง</h1>
+          <input type="number" className='border rounded-lg border-blue-900 w-full' value={Amount} onChange={(e) => {
+            const value = e.target.value
+            if (/^\d*$/.test(value)) { // regex เช็กว่าเป็นตัวเลขล้วน
+              setAmount(Number(value))
+            }
+          }} placeholder='กรอกจำนวนเงิน...' />
+          <button className='bg-blue-500 hover:bg-blue-600 text-white rounded-full px-4 py-2' onClick={handleCreateQrCode}>gennerate qe code</button>
+        </div>
+        <div className="flex flex-col items-center gap-4">
+          <h1 className='text-2xl font-semibold'>QR CODE คเชนทร์ ชัยเทศ</h1>
+          <img src={qr.qr} alt="" className='w-[200px] h-[200px] bg-blue-900' />
+          <button
+            className='flex justify-center gap-2 items-center bg-green-500 text-white px-4 py-2 rounded-lg'
+            onClick={() => {
+              if (!qr.qr) return
+              const link = document.createElement('a')
+              link.href = qr.qr          // base64 image
+              link.download = 'promptpay-qr.png'
+              link.click()
+            }}
+          >
+            <Download /> Download
+          </button>
+
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
